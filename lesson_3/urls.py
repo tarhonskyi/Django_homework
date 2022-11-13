e8bb138e13b48f900c1944e9097d5cfde0012bf8
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2022-11-13 19:37:59 +0200

    add some sollutions to lesson_3

--- lesson_3/urls.py
+++ lesson_3/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import task_1, task_2, luk, lea, khan
+from .views import task_1, task_2, luk, lea, khan, task_5
 
 app_name = 'lesson_3'
 
 urlpatterns = [
     path('task_1/', task_1, name='task_3_1'),
-    path('task_2/', task_2, name='main'),
+    path('task_2/', task_2, name='task_2'),
     path('task_2/luk/', luk, name='luk'),
     path('task_2/lea/', lea, name='lea'),
     path('task_2/khan/', khan, name='khan'),
+    path('task_5/', task_5, name='task_5'),
 ]
--- lesson_3/views.py
+++ lesson_3/views.py
@@ -10,7 +10,7 @@ def task_1(request):
 
 
 def task_2(request):
-    return render(request, 'lesson_3/main.html')
+    return render(request, 'lesson_3/task_2.html')
 
 
 def luk(request):
@@ -34,3 +34,12 @@ def khan(request):
                         <p>Хан. Соло — пілот космічного корабля «Тисячолітній сокіл», його бортмеханіком та другим 
                         пілотом є вуки на ім'я Чубакка.</p>
                         """)
+
+
+def task_5(request):
+    lets_do_it = [{'priority': 100, 'task': 'Скласти перелік справ'},
+                  {'priority': 150, 'task': 'Вивчати Django'},
+                  {'priority': 1, 'task': 'Подумати про сенс життя'}]
+    context = {"do_list": lets_do_it}
+    return render(request, 'lesson_3/task_5.html', context)
+
