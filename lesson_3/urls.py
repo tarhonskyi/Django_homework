236e21ae0ad399325b40972095c694cd41e44ce3
Vladyslav Tarhonskyi <vladislav.targonskii@gmail.com>
2022-11-15 12:34:02 +0200

    made homework_3

--- lesson_3/urls.py
+++ lesson_3/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import task_1, task_2, luk, lea, khan, task_5
+from .views import (task_1, task_2, luk, lea, khan,
+                    task_3, task_3_file, task_5, task_6,
+                    task_7)
 
 app_name = 'lesson_3'
 
@@ -9,5 +11,10 @@ urlpatterns = [
     path('task_2/luk/', luk, name='luk'),
     path('task_2/lea/', lea, name='lea'),
     path('task_2/khan/', khan, name='khan'),
+    path('task_3/', task_3, name='task_3'),
+    path('task_3/file/', task_3_file, name='file'),
     path('task_5/', task_5, name='task_5'),
+    path('task_6/', task_6, name='task_6'),
+    path('task_7/polls/', task_7, name='task_7'),
+    path('task_7/polls/<int:question_id>/', task_7, name='question_detail')
 ]
--- lesson_3/views.py
+++ lesson_3/views.py
@@ -6,7 +6,7 @@ def task_1(request):
     lets_do_it = [{'priority': 100, 'task': 'Скласти перелік справ'},
                   {'priority': 150, 'task': 'Вивчати Django'},
                   {'priority': 1, 'task': 'Подумати про сенс життя'}]
-    return HttpResponse('Plan\n' + str(lets_do_it))
+    return HttpResponse(f'<h1>Plan</h1><p>{str(lets_do_it)}</p>')
 
 
 def task_2(request):
@@ -36,10 +36,48 @@ def khan(request):
                         """)
 
 
+def task_3(request):
+    return render(request, 'lesson_3/task_3.html')
+
+
+def task_3_file(request):
+    with open('lesson_3/answer.txt', 'rb') as file:
+        data = file.read()
+    content = data
+    response = HttpResponse(content, status=227, content_type='application/msword')
+    response['Content-Disposition'] = 'attachment; filename="answer.doc"'
+    return response
+
+
 def task_5(request):
     lets_do_it = [{'priority': 100, 'task': 'Скласти перелік справ'},
                   {'priority': 150, 'task': 'Вивчати Django'},
                   {'priority': 1, 'task': 'Подумати про сенс життя'}]
     context = {"do_list": lets_do_it}
     return render(request, 'lesson_3/task_5.html', context)
+
+
+def task_6(request):
+    persons = [
+        {'name': 'Шаддам IV', 'surname': 'Корріно'},
+        {'name': 'Стать', 'surname': 'Атрейдес'},
+        {'name': 'Франклін', 'surname': 'Герберт'}]
+    context = {'persons': persons}
+    return render(request, 'lesson_3/task_6.html', context)
+
+
+def task_7(request, question_id=None):
+    latest_question_list = [{'id': 1, 'question_text': 'У чому сенс життя?'},
+                            {'id': 2, 'question_text': 'Що первинне: дух чи матерія?'},
+                            {'id': 3, 'question_text': 'Чи існує свобода волі?'}]
+    # latest_question_list = []
+    if question_id:
+        for question in latest_question_list:
+            if question['id'] == question_id:
+                context = {'question': question}
+                return render(request, 'lesson_3/detail.html', context)
+        else:
+            return HttpResponse(f'Питання під таким номером {question_id} немає в списку')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'lesson_3/task_7.html', context)
 
